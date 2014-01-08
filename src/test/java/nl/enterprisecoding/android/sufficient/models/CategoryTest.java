5e2a0461e51304b1131453eb8862222aaadba44e
Ferry Wienholts <f.wienholts@hotmail.com>
2014-01-08 10:29:12 +0100

    new test files added!

--- src/test/java/nl/enterprisecoding/android/sufficient/models/CategoryTest.java
+++ src/test/java/nl/enterprisecoding/android/sufficient/models/CategoryTest.java
@@ -1,11 +1,4 @@
 package nl.enterprisecoding.android.sufficient.models;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Gebruiker
- * Date: 8-1-14
- * Time: 10:11
- * To change this template use File | Settings | File Templates.
- */
 public class CategoryTest {
 }
