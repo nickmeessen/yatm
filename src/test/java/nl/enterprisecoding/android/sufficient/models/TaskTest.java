12298b628f61b7319f4a64366f973cb3b496ee0a
Ferry Wienholts <f.wienholts@hotmail.com>
2014-01-08 10:25:42 +0100

    testing commit

---
+++ src/test/java/nl/enterprisecoding/android/sufficient/models/TaskTest.java
@@ -0,0 +1,11 @@
+package nl.enterprisecoding.android.sufficient.models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gebruiker
+ * Date: 8-1-14
+ * Time: 10:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class TaskTest {
+}
