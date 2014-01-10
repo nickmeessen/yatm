162d3339c5f5b912a1c629fb687fff2e1fe72cdf
Nick Meessen <nickmeessen@icloud.com>
2014-01-10 14:02:55 +0100

    Fixed #TEST-453.

--- src/main/java/nl/enterprisecoding/android/sufficient/activities/TaskActivity.java
+++ src/main/java/nl/enterprisecoding/android/sufficient/activities/TaskActivity.java
@@ -153,7 +153,7 @@ public class TaskActivity extends MainActivity {
             startActivity(intent);
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         } else {
-            long newTaskID = mTaskManager.createTask("New Task", catList.get(0).getId(), Calendar.getInstance(), false);
+            long newTaskID = mTaskManager.createTask("", mCurrentCategoryID, Calendar.getInstance(), false);
 
             Intent intent = new Intent(this, EditTaskActivity.class);
             intent.putExtra(TASK_ID, newTaskID);
