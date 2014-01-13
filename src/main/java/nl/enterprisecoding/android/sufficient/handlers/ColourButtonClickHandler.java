3d6dfca3c80e0322f564db08e540f4d06779c505
Sjors Roelofs <sjors.roelofs@gmail.com>
2014-01-13 13:43:58 +0100

    Removed onClick for colourButton in MainActivity and added ClickHandler for colourButton

---
+++ src/main/java/nl/enterprisecoding/android/sufficient/handlers/ColourButtonClickHandler.java
@@ -0,0 +1,51 @@
+package nl.enterprisecoding.android.sufficient.handlers;
+
+import android.app.Dialog;
+import android.graphics.drawable.GradientDrawable;
+import android.view.View;
+import nl.enterprisecoding.android.sufficient.activities.MainActivity;
+
+/**
+ * ClickHandler for the Colour button
+ *
+ * @author Sjors Roelofs
+ */
+public class ColourButtonClickHandler implements IButtonClickHandler {
+
+    private MainActivity mActivity;
+    private int mInputColour;
+    private GradientDrawable mBgShape;
+    private Dialog mColourDialog;
+    private int[] mRandColour;
+
+    public void setData(MainActivity activity, int inputColour, GradientDrawable bgShape, Dialog colourDialog, int[] randColour) {
+        setActivity(activity);
+
+        mInputColour = inputColour;
+        mBgShape = bgShape;
+        mColourDialog = colourDialog;
+        mRandColour = randColour;
+    }
+
+    @Override
+    public void setActivity(MainActivity activity) {
+        mActivity = activity;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int chosenColour;
+
+        if (mInputColour != 0) {
+            chosenColour = mActivity.getResources().getColor(mInputColour);
+            mBgShape.setColor(mActivity.getResources().getColor(mInputColour));
+        } else {
+            chosenColour = mRandColour[0];
+            mBgShape.setColor(mRandColour[0]);
+        }
+
+        mActivity.setFinalColour(chosenColour);
+        mColourDialog.dismiss();
+    }
+
+}
