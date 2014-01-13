39da55911d23796d0b114eaee0cc0a3c2c9ad13c
Sjors Roelofs <sjors.roelofs@gmail.com>
2014-01-13 15:45:17 +0100

    Added test_toString() in TaskTest

--- src/test/java/nl/enterprisecoding/android/sufficient/models/TaskTest.java
+++ src/test/java/nl/enterprisecoding/android/sufficient/models/TaskTest.java
@@ -16,6 +16,7 @@ import org.robolectric.RobolectricTestRunner;
 import java.util.Calendar;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 public class TaskTest {
@@ -67,8 +68,16 @@ public class TaskTest {
     }
 
     @Test
-    public void Test_setCompletedIsCompleted() {
+    public void test_setCompletedIsCompleted() {
         task.setCompleted(true);
         assertTrue(task.isCompleted());
+    }
+
+    @Test
+    public void test_toString() {
+        String title = Mockito.anyString();
+        task.setTitle(title);
+
+        assertEquals(title, task.toString());
     }
 }
