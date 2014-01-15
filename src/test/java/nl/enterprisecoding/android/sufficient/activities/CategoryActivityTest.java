f4145995a1a3e0f9ee2778bdc9cf978d9d349bef
Nick Meessen <nickmeessen@icloud.com>
2014-01-15 10:46:46 +0100

    Work done on Unit Tests. Fixed bug in TaskManager.

--- src/test/java/nl/enterprisecoding/android/sufficient/activities/CategoryActivityTest.java
+++ src/test/java/nl/enterprisecoding/android/sufficient/activities/CategoryActivityTest.java
@@ -1,6 +1,11 @@
 package nl.enterprisecoding.android.sufficient.activities;
 
+import android.content.Intent;
+import android.view.ContextMenu;
 import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import nl.enterprisecoding.android.sufficient.R;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +15,8 @@ import org.robolectric.shadows.ShadowActivity;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /*
  * Copyright (c) 2013. "EnterpriseCoding"
@@ -21,25 +28,89 @@ import static org.junit.Assert.assertTrue;
 @RunWith(RobolectricTestRunner.class)
 public class CategoryActivityTest {
 
-    private CategoryActivity mCategoryActivity;
+    private CategoryActivity mCategoryActivity1;
+    private CategoryActivity mCategoryActivity2;
 
     @Before
     public void setUp() {
-        mCategoryActivity = Robolectric.buildActivity(CategoryActivity.class).create().get();
+
+        Intent intent = new Intent(new CategoryActivity(), CategoryActivity.class);
+        intent.putExtra(MainActivity.CATEGORY_ID, 3);
+
+        mCategoryActivity1 = Robolectric.buildActivity(CategoryActivity.class).withIntent(intent).create().get();
+        mCategoryActivity2 = Robolectric.buildActivity(CategoryActivity.class).create().get();
     }
 
     @Test
 
     public void test_onKeyUp() {
-        assertTrue(mCategoryActivity.onKeyUp(KeyEvent.KEYCODE_MENU, null));
-        assertFalse(mCategoryActivity.onKeyUp(KeyEvent.KEYCODE_0, null));
+        assertTrue(mCategoryActivity1.onKeyUp(KeyEvent.KEYCODE_MENU, null));
+        assertFalse(mCategoryActivity2.onKeyUp(KeyEvent.KEYCODE_0, null));
+    }
+
+
+    @Test
+    public void test_onBackPressed1() {
+        mCategoryActivity1.onBackPressed();
+        assertTrue(mCategoryActivity1.isFinishing());
     }
 
     @Test
-    public void test_onBackPressed() {
-        mCategoryActivity.onBackPressed();
-        ShadowActivity shadowActivity = Robolectric.shadowOf(mCategoryActivity);
+    public void test_onBackPressed2() {
+        mCategoryActivity2.onBackPressed();
+        assertTrue(mCategoryActivity2.isFinishing());
+    }
 
-        assertTrue(shadowActivity.isFinishing());
+
+    @Test
+    public void test_onCreateContextMenu1() {
+
+        ContextMenu menu = mock(ContextMenu.class);
+        View view = mock(View.class);
+        ContextMenu.ContextMenuInfo menuInfo = mock(ContextMenu.ContextMenuInfo.class);
+
+        when(view.getId()).thenReturn(R.id.cat_list);
+
+        mCategoryActivity1.onCreateContextMenu(menu, view, menuInfo);
+
+    }
+
+    @Test
+    public void test_onCreateContextMenu2() {
+
+        ContextMenu menu = mock(ContextMenu.class);
+        View view = mock(View.class);
+        ContextMenu.ContextMenuInfo menuInfo = mock(ContextMenu.ContextMenuInfo.class);
+
+        when(view.getId()).thenReturn(R.id.catText);
+
+        mCategoryActivity1.onCreateContextMenu(menu, view, menuInfo);
+
+    }
+
+
+
+    @Test
+    public void test_onContextItemSelected1() {
+
+        MenuItem menuItem = mock(MenuItem.class);
+
+        when(menuItem.getTitle()).thenReturn(mCategoryActivity1.getString(R.string.action_edit));
+
+        assertTrue(mCategoryActivity1.onContextItemSelected(menuItem));
+        assertTrue(mCategoryActivity2.onContextItemSelected(menuItem));
+
+    }
+
+    @Test
+    public void test_onContextItemSelected2() {
+
+        MenuItem menuItem = mock(MenuItem.class);
+
+        when(menuItem.getTitle()).thenReturn(mCategoryActivity1.getString(R.string.toast_no_category));
+
+        assertTrue(mCategoryActivity1.onContextItemSelected(menuItem));
+        assertTrue(mCategoryActivity2.onContextItemSelected(menuItem));
+
     }
 }
